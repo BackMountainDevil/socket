a8c4e8d4a41542c27931cea1268d01b7ccdec137
kearney <191615342@qq.com>
2021-07-07 23:14:41 +0800

    add 迭代回声 CS 通信

---
+++ loop/README.md
@@ -0,0 +1,61 @@
+# 迭代回声 CS 通信
+
+之前的服务端、客户端允许一下就结束了，这里再修改一下，能让两者都保持允许。实现两者保持运行只要加个循环即可，除非输入特定指令结束程序。这个程序实现了 CS 一对一回声，还不支持多个客户端，而且一个客户端也是由不断杀死新生的小小 socket 组成的。
+- 迭代服务端：循环 accept, read/write, close
+- 回声：服务端将收到的数据发回给客户端
+- 迭代客户端：循环 socket, connect, write/read, close；直到用户输入 “\q” 才结束程序，否则就一直将客户输入的信息发给服务端
+
+- [实现迭代服务器端和客户端](http://c.biancheng.net/cpp/html/3039.html)
+
+## Linux socket 
+- server.cpp: 服务端程序
+- client.cpp: 客户端程序
+### 如何运行
+
+[正常运行步骤](../basic/README.md)和前面的一致，这里客户端输入 ‘\q’ 则会结束程序，服务端需要 Ctrl + c 来手动关闭。一个可能的输出结果如下
+
+```bash
+$ ./server 
+Waiting for connecting
+New client：4 , IP 127.0.0.1 , Port 38030
+4：asd
+New client：4 , IP 127.0.0.1 , Port 38032
+4：asdv
+New client：4 , IP 127.0.0.1 , Port 38034
+4：\q
+```
+
+另一个终端
+
+```bash
+$ ./client 
+Input: asd
+Recv: asd
+Input: asdv
+Recv: asdv
+Input: \q
+Log: Output close
+```
+
+### 函数解释
+
+- shutdown  
+    `int shutdown (int __fd, int __how)`  
+    定义在 <socket.h> 中，作用是将套接字（__fd）的某些功能关闭。失败时返回 -1,成功时返回 0  
+    __how：SHUT_RD 代表关闭输入流，SHUT_WR 关闭输出流， SHUT_RDWR 则是都关了
+
+- memset  
+    `void *memset (void *__s, int __c, size_t __n)`  
+    定义在 <string.h> 中，作用是将 `__s` 的前 `__n` 个字节重置为 `__c`。
+
+- cin.getline  
+    `__istream_type& getline(char_type* __s, streamsize __n)`  
+    定义在 <istream> 中，作用是将输入流的 `__n` 个字节的数据存到 ` __s` 中。由于scanf() 读取到空格时认为输入结束，考虑下 fgets() 或者 std::cin.getline()。gets() 已经被 C11 标准抛弃，不建议使用  
+    ```bash
+    scanf("%s", buffer);
+    fgets(buffer,fgets(buffer,BUF_SIZE,stdin);,stdin);    //  C: fgets() 会读取换行符'\n'，除非缓存区不够大
+    std::cin.getline(buffer, BUF_SIZE); // C++
+    ```
+
+    - [c语言gets()函数与它的替代者fgets()函数](https://www.cnblogs.com/qingergege/p/5925811.html)
+    - [PAT B1009 error: ‘gets’ was not declared in this scope gets(s)之解决办法 一只小菜猪 2019-01-24](https://blog.csdn.net/qq_36525099/article/details/86631881)
---
+++ loop/client.cpp
@@ -0,0 +1,75 @@
+#include <arpa/inet.h>
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1025
+#define IP "127.0.0.1"
+#define PORT 8080
+
+int main() {
+
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr)); //每个字节都用0填充
+  serv_addr.sin_family = AF_INET;           //使用IPv4地址
+  serv_addr.sin_port = htons(PORT);         //端口
+
+  if (inet_pton(AF_INET, IP, &serv_addr.sin_addr) <= 0) { // IP 地址绑定和检查
+    perror("Error: Invalid address/ Address not supported");
+    exit(EXIT_FAILURE);
+  }
+
+  // 输出、输入缓冲区
+  char bufSend[BUF_SIZE] = {0};
+  char bufRecv[BUF_SIZE] = {0};
+
+  while (true) {
+    // 创建套接字
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+      perror("Error: Socket creation failed");
+      close(sock);
+      exit(EXIT_FAILURE);
+    }
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("Error: Connection creation failed");
+      close(sock);
+      exit(EXIT_FAILURE);
+    }
+
+    // 获取用户输入的字符串并发送给服务端
+    std::cout << "Input: ";
+    std::cin.getline(bufSend, BUF_SIZE);
+    if (write(sock, bufSend, sizeof(bufSend)) < 0) {
+      perror("Error: Send fail\n");
+      close(sock);
+      exit(EXIT_FAILURE);
+    }
+
+    // 输入 ‘\q’ ,逐步终止程序
+    if (!strcmp(bufSend, "\\q")) {
+      shutdown(sock, SHUT_WR); // 关闭输出流
+      printf("Log: Output close\n");
+      read(sock, bufRecv, sizeof(bufRecv));
+      close(sock);
+      exit(EXIT_FAILURE);
+    }
+
+    // 读取服务器传回的数据
+    if (read(sock, bufRecv, sizeof(bufRecv)) < 0) {
+      perror("Error: Receive fail");
+      close(sock);
+      exit(EXIT_FAILURE);
+    }
+    std::cout << "Recv: " << bufRecv << std::endl;
+    close(sock);
+    // 重置缓冲区
+    memset(bufSend, 0, BUF_SIZE);
+    memset(bufRecv, 0, BUF_SIZE);
+  }
+  printf("Message form server: %s\n", bufRecv);
+
+  // 关闭套接字
+  printf("Client close\n");
+  return 0;
+}
